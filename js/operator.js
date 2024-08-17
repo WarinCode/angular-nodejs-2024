b15897ee90adb46008b5877b48b67b7c07aac675
WarinCode <warinsaipanya471@outlook.co.th>
2024-08-18 00:13:19 +0700

    Learn day2.

---
+++ js/operator.js
@@ -0,0 +1,11 @@
+console.log(5 + 5);
+console.log(5 - 5);
+console.log(5 * 5);
+console.log(10 / 3); // 3.33
+console.log(5 % 2); // 
+
+let x = 5;
+x++;
+console.log(x); // เพิ่มค่าขึ้นอีก 1
+x--;
+console.log(x); // ลดค่าอีก 1
