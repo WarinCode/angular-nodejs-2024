3952a43541128655843114c699577098a9039472
WarinCode <warinsaipanya471@outlook.co.th>
2024-09-12 21:26:43 +0700

    Learn day16

--- workshop/api/server.js
+++ workshop/api/server.js
@@ -7,14 +7,14 @@ const userController = require("./controllers/UserController.js");
 const foodTypeController = require("./controllers/FoodTypeController.js")
 
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/api/foodType/list", (req, res) => foodTypeController.list(req, res))
+app.put("/api/foodType/update", (req, res) => foodTypeController.update(req, res));
+app.delete("/api/foodType/remove/:id", (req, res) => foodTypeController.remove(req, res));
+app.get("/api/foodType/list", (req, res) => foodTypeController.list(req, res));
 app.post("/api/foodType/create", (req, res) => foodTypeController.create(req, res));
 app.post("/api/user/signIn", (req, res) => userController.sigin(req, res));
-
 
 app.listen(3000, () => {
     console.log("API Server Running ...");
--- workshop/web/app/src/app/food-type/food-type.component.ts
+++ workshop/web/app/src/app/food-type/food-type.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 import config from '../../config';
+import { MyModalComponent } from '../my-modal/my-modal.component';
 
 @Component({
   selector: 'app-food-type',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MyModalComponent],
   templateUrl: './food-type.component.html',
   styleUrl: './food-type.component.css',
 })
@@ -15,21 +16,41 @@ export class FoodTypeComponent {
   name: string = '';
   remark: string = '';
   foodTypes: any = [];
+  id: number = 0;
 
   constructor(private http: HttpClient) {}
+
+  clearForm(){
+    this.name = "";
+    this.remark = "";
+    this.id = 0;
+  }
 
   save() {
     try {
       const payload = {
         name: this.name,
         remark: this.remark,
+        id: 0,
       };
 
-      this.http
-        .post(config.apiServer + '/api/foodType/create', payload)
-        .subscribe((res: any) => {
-          this.fetchData();
-        });
+      if (this.id > 0) {
+        payload.id = this.id;
+        this.http
+          .put(config.apiServer + '/api/foodType/update', payload)
+          .subscribe((res: any) => {
+            this.fetchData();
+            this.id = 0
+          });
+      } else {
+        this.http
+          .post(config.apiServer + '/api/foodType/create', payload)
+          .subscribe((res: any) => {
+            this.fetchData();
+          });
+      }
+
+      document.getElementById("modalFoodType_btnClose")?.click(); 
     } catch (e: any) {
       Swal.fire({
         title: 'error',
@@ -49,5 +70,37 @@ export class FoodTypeComponent {
       .subscribe((res: any) => {
         this.foodTypes = res.results;
       });
+  }
+
+  async remove(item: any) {
+    try {
+      const button = await Swal.fire({
+        title: 'ลบรายการ',
+        text: 'คุณต้องการลบรายการใช่หรือไม่',
+        icon: 'question',
+        showCancelButton: true,
+        showConfirmButton: true,
+      });
+
+      if (button.isConfirmed) {
+        this.http
+          .delete(config.apiServer + '/api/foodType/remove/' + item.id)
+          .subscribe((res: any) => {
+            this.fetchData();
+          });
+      }
+    } catch (e: any) {
+      Swal.fire({
+        title: 'error',
+        text: e.message,
+        icon: 'error',
+      });
+    }
+  }
+
+  edit(item: any) {
+    this.name = item.name;
+    this.remark = item.remark;
+    this.id = item.id;
   }
 }
